f077b1cdf8d8694a59f62bf2a049b00b206dfa71
SSnuwan00 <srisanjukanuwan@gmail.com>
2025-06-16 12:41:09 +0530

    updated

--- src/main/java/com/example/task_manager_backend/repository/TaskRepository.java
+++ src/main/java/com/example/task_manager_backend/repository/TaskRepository.java
@@ -3,11 +3,15 @@ package com.example.task_manager_backend.repository;
 import com.example.task_manager_backend.dto.TaskDetailsDTO;
 import com.example.task_manager_backend.dto.response.TaskDetailsResponseDTO;
 import com.example.task_manager_backend.model.Task;
+import com.example.task_manager_backend.model.enums.TimeStatus;
+import com.example.task_manager_backend.model.enums.UserStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -36,4 +40,16 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
             "JOIN t.taskStatus ts " +
             "WHERE t.taskId = :taskId")
     Optional<TaskDetailsResponseDTO> findAllDetailsById(Long taskId);
+
+    @Modifying
+    @Query("UPDATE Task t SET t.title = :title, t.description = :description, t.startDate = :startDate, " +
+            "t.startTime = :startTime, t.endDate = :endDate, t.endTime = :endTime WHERE t.taskId = :taskId")
+    int updateDetailsById(@Param("taskId") Long taskId,
+                          @Param("title") String title,
+                          @Param("description") String description,
+                          @Param("startDate") String startDate,
+                          @Param("startTime") String startTime,
+                          @Param("endDate") String endDate,
+                          @Param("endTime") String endTime);
+
 }
